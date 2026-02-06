84df798a7c92d98633bfd6f499b62a96c6fc8b60
semvlu <74308718+semvlu@users.noreply.github.com>
2026-02-06 12:48:06 +0800

    Delete Terraform/outputs.tf

--- Terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "EC2 ID"
-  value = aws_instance.app_server.id
-}
-
-output "instance_public_ip" {
-  description = "EC2 public IP address"
-  value = aws_instance.app_server.public_ip
-}
