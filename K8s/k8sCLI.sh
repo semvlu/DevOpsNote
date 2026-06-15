0304ad11898d60e7c361ee82ea0f24c4789b2585
semvlu <74308718+semvlu@users.noreply.github.com>
2026-06-15 15:43:58 +0800

    Update k8sCLI.sh

--- K8s/k8sCLI.sh
+++ K8s/k8sCLI.sh
@@ -1,21 +1,45 @@
-kubectl create -f ./k8s.yml
-kubectl get rc # check replication controller status
-ibid. rs # replica set status
-kubectl describe
-kubectl delete pod <podName> # delete a pod 
-kubectl scale --replicas=4 -f ./k8s.yml # scale #pods
+kubectl create -f k8s.yml
+kubectl get [all, rc, po,...]  # rc: replication controller
 
-kubectl delete rc <replication-controllerName> --cascade=false
-# pods will still work after delete controller
-# Most use replicaSet/Deployment insted of replication-controller
+# Get #resources
+kubectl get all --no-headers | wc -l
 
-kubectl set image deploy/<deployName> \
-<podName>:<img-path>:<version> # e.g. <img-path>: dockerUsr/repos
+# Gen template yml
+kubectl run <pod-name> --image=busybox -n <ns> --dry-run=client -o yaml > pod.yml
+kubectl create deployment <deploy-name> \
+--image=nginx \
+--replicas=3 \
+--dry-run=client -o yaml > deployment.yml
+
+kubectl delete pod <pod-name> # delete a pod 
+kubectl scale --replicas=4 -f k8s.yml # scale #pods
+
+# List all images running in a cluster
+kubectl get pods -A -o custom-columns='DATA:spec.containers[*].image'
+
+# List all images running, group by Pod
+kubectl get pods -A -o custom-columns='NAME:.metadata.name,IMAGE:.spec.containers[*].image'
+
+# Reference 
+kubectl api-resources
+kubectl explain <resource-kind> --recursive # Align w/ yaml fields
+kubectl explain pod.spec.containers
+
+kubectl delete rc <replication-controller-name> --cascade=false
+# Pods will still work after controller deleted
+# Most use replicaSet/Deployment over replication-controller
+
+kubectl set image deploy <deploy-name> \
+<pod-name>:<img-path>:<version> # e.g. <img-path>: dockerUsr/repos
 # update image of a pod in a deployment
 
-kubectl expose deploy <deployName> --type=NodePort --name=<serviceName>
-# make a name for serviceName
-minikube service <serviceName> --url # get service url
+kubectl expose deploy <deploy-name> \
+--name=<service-name>
+--port=80 \
+--target-port=80 \
+--type=NodePort 
+
+minikube service <service-name> --url # get service url
 
 
 # Deployment
@@ -23,7 +47,9 @@ kubectl scale deploymenmt --replicas=1 <container> <container>...
 # Scale down
 --replicas=0
 # Each container as a service
-# my-deploymenmt is optional
+
+# HPA
+kubectl autoscale deploy <deploy-name> --min=2 --max=5
 
 
 # apply config (declarative)
@@ -31,49 +57,27 @@ kubectl apply -f <filepath> # pod/service/deployment/ingress etc. .yml
 # delete config
 kubectl delete -f <filepath>
 
+# Secret 
+kubectl create secret <type>
+kubectl create secret generic <secret> \
+--from-literal=<k>=<v> \ 
+--from-file=/path/to/file
+# <type>: docker-registry, generic, tls
+
+kubectl get secret <secret> -o yaml
+
+
+kubectl taint nodes <node> <key>=<val>:<taint-effect>
+# <taint-effect>: NoSchedule, PreferNoSchedule, NoExecute (Existing pods will be evicted if intolerable to the taint)
+
+# change ns to default
+kubectl config set-context --current -n default
 
 
 
-
-# Context: a group of acc param to a k8s cluster
-# Incl.: cluster, usr, namespace
-
-kubectx <contextName> # kubectl config use-context
-
-# get namespace
-kubectl get ns
-
-# get pods from default ns
-kubectl get pods --namespace=kube-system
-
-# change to default ns
-kubectl config set-context --current --namespace=default
-
-
-:'
-Master node: aka control plane, do not run containers in the master
-incl. kube-controller-manager
-      cloud-controller-manager
-      kube-scheduler: watch new pods which not yet assigned to a node
-      select a node to run on
-
-      etcd: store cluster state 
-      kube-apiserver interact w/ etcd (datastore)
-
-Worker node
-incl. kubelet: run containers on the node
-      kube-proxy: manage net rules on nodes
-      container runtime
-'
-
-:'
-Init container
-app has depend. on A, but not to tie A w/ infra code
-init a pod and remove init container before app container run 
-'
 
 kubectl get po -o wide # pod IP adr
-kubectl get ep <serviceName> # service endpoint, from service metadata.name
+kubectl get endpointslice <service-name> # service endpoint, from service metadata.name
 kubectl port-forward <kind>/<name> 8080:80
 
 
@@ -91,5 +95,5 @@ kubectl delete pvc www-nginx-sts-1
 kubectl delete pvc www-nginx-sts-2
 
 # Rollback to prev ver of deployment
-kubectl rollout undo deployment <deploymentName> --to-revision=<revisionNumber>
+kubectl rollout undo deploy <deplo-name> --to-revision=<revisionNumber>
 
