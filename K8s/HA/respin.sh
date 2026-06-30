9c3eabf5b0518511d2a99df4d02eed168dfa9909
semvlu <74308718+semvlu@users.noreply.github.com>
2026-06-30 15:42:22 +0800

    Update respin.sh

--- K8s/HA/respin.sh
+++ K8s/HA/respin.sh
@@ -2,3 +2,6 @@
 mv /etc/kubernetes/manifests/haproxy.yaml /tmp/
 sleep 10
 mv /tmp/haproxy.yaml /etc/kubernetes/manifests/
+
+kubectl patch deploy rancher -n cattle-system \
+  -p '{"spec": {"template": {"spec": {"containers": [{"name": "rancher", "startupProbe": {"failureThreshold": 300}}]}}}}'
